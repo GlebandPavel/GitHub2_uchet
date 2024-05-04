ad6397006a4cc84a9bb3d422f5a17a987f773723
Polyat Polyattrif Trif <polyattrif@iMac-Administrator-2.local>
2024-05-04 15:16:23 +0900

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.util.List;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private ProgressBar mProgressBar;
+    private TextView mTextView;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        mTextView = (TextView) findViewById(R.id.textView);
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+        mProgressBar.setVisibility(View.INVISIBLE);
+    }
+
+
+    public void onClick(View view) {
+        mProgressBar.setVisibility(View.VISIBLE);
+
+        GitHubService gitHubService = GitHubService.retrofit.create(GitHubService.class);
+        final Call<User> call =
+                gitHubService.getUser("GoodwinSEV");
+
+        call.enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+                // response.isSuccessfull() is true if the response code is 2xx
+                if (response.isSuccessful()) {
+                    User user = response.body();
+
+                    // Получаем json из github-сервера и конвертируем его в удобный вид
+                    mTextView.setText("Аккаунт Github: " + user.getName() +
+                            "\nСайт: " + user.getBlog() +
+                            "\nКомпания: " + user.getCompany());
+
+                    mProgressBar.setVisibility(View.INVISIBLE);
+                } else {
+                    int statusCode = response.code();
+
+                    // handle request errors yourself
+                    ResponseBody errorBody = response.errorBody();
+                    try {
+                        mTextView.setText(errorBody.string());
+                        mProgressBar.setVisibility(View.INVISIBLE);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<User> call, Throwable throwable) {
+                mTextView.setText("Что-то пошло не так: " + throwable.getMessage());
+            }
+        });
+    }
+}
